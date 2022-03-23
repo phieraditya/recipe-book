6d861cc37f790620a867f0c8f5d8a684998bd26a
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-03-23 15:57:12 +0700

    loading a recipe from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,3 +8,33 @@ const timeout = function (s) {
     });
   });
 };
+
+//////////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca36'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    console.log(res, data);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+showRecipe();
