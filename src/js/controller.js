85e7613dd5c5e7929159d4c54245b621c81b4f98
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-03-24 13:24:03 +0700

    Rendering spinner
    -At components.scss, fix typo on spinner styling
    -At controller.js, add spinner while loading recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -15,9 +15,23 @@ const timeout = function (s) {
 
 //////////////////////////////////////////
 
+const renderSpinner = function (parentEl) {
+  const markup = `
+    <div class="spinner">
+      <svg>
+        <use href="src/img/icons.svg#icon-loader"></use>
+      </svg>
+    </div>
+  `;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
+
 const showRecipe = async function () {
   try {
     // 1) Loading recipe
+    renderSpinner(recipeContainer);
+
     const res = await fetch(
       // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca36'
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc7e'
