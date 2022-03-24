b22dab117ce25ee21a5fa46f670694ceaa9c6fdb
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-03-24 15:36:02 +0700

    listening for hashchange and load events

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,18 +30,20 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     // 1) Loading recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca36'
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc7e'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
 
-    console.log(res, data);
     let { recipe } = data.data;
     recipe = {
       id: recipe.id,
@@ -159,4 +161,5 @@ const showRecipe = async function () {
     alert(err);
   }
 };
-showRecipe();
+
+['hashchange', 'load'].forEach((ev) => window.addEventListener(ev, showRecipe));
