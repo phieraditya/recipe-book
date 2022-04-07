c4c8423fdd79137813ec8322a92809bd309530f3
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-04-07 19:02:32 +0700

    refactor generate markup ingredient

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -87,24 +87,7 @@ class RecipeView {
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe Ingredients</h2>
           <ul class="recipe__ingredient-list">
-          ${this.#data.ingredients
-            .map((ing) => {
-              return `
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${
-                ing.quantity ? new Fraction(ing.quantity).toString() : ''
-              }</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${ing.unit}</span>
-                ${ing.description}
-              </div>
-            </li>
-            `;
-            })
-            .join('')}
+          ${this.#data.ingredients.map(this.#generateMarkupIngredient).join('')}
             
           </ul>
         </div>
@@ -130,6 +113,23 @@ class RecipeView {
           </a>
         </div>
     `;
+  }
+
+  #generateMarkupIngredient(ing) {
+    return `
+  <li class="recipe__ingredient">
+    <svg class="recipe__icon">
+      <use href="${icons}#icon-check"></use>
+    </svg>
+    <div class="recipe__quantity">${
+      ing.quantity ? new Fraction(ing.quantity).toString() : ''
+    }</div>
+    <div class="recipe__description">
+      <span class="recipe__unit">${ing.unit}</span>
+      ${ing.description}
+    </div>
+  </li>
+  `;
   }
 }
 
