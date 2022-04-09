88d5cb37e0cea8c96020d92e70c1210e5356c3d4
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-04-09 16:24:59 +0700

    set default error message
    To keep the error message as an intrinsic property of the View, add private field (errorMessage) in the View and pass it to the renderError method as the default error message. So, the message argument is set by default.

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -4,6 +4,7 @@ import { Fraction } from 'fractional';
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
+  #errorMessage = 'We could not find that recipe. Please try another one!';
 
   render(data) {
     this.#data = data;
@@ -24,7 +25,7 @@ class RecipeView {
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
-  renderError(message) {
+  renderError(message = this.#errorMessage) {
     const markup = `
       <div class="error">
         <div>
