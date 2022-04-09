f3db78ecd3fdca884164f2ba3b3a2093fd19e4fe
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-04-09 16:13:23 +0700

    display error message in the view

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -12,17 +12,32 @@ class RecipeView {
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
-  renderSpinner = function () {
+  renderSpinner() {
     const markup = `
-    <div class="spinner">
-    <svg>
-    <use href="src/img/icons.svg#icon-loader"></use>
-    </svg>
-    </div>
+      <div class="spinner">
+        <svg>
+        <use href="${icons}#icon-loader"></use>
+        </svg>
+      </div>
     `;
     this.#clear();
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-  };
+  }
+
+  renderError(message) {
+    const markup = `
+      <div class="error">
+        <div>
+          <svg>
+            <use href="${icons}#icon-alert-triangle"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach((ev) =>
