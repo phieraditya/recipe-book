ac87cbee98b8d21175080f38f7d17ee9cb856d69
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-04-10 13:38:12 +0700

    implement search results: search state and control

--- src/js/controller.js
+++ src/js/controller.js
@@ -25,6 +25,16 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    await model.loadSearchResults('potato');
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+controlSearchResults();
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
 };
