26e0dffc07899ac80f00ec78bd9439bc0ea4e0c3
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-04-10 15:42:47 +0700

    implement search results: clear search input field

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,7 +2,13 @@ class SearchView {
   #parentElement = document.querySelector('.search');
 
   getQuery() {
-    return this.#parentElement.querySelector('.search__field').value;
+    const query = this.#parentElement.querySelector('.search__field').value;
+    this.#clearInput();
+    return query;
+  }
+
+  #clearInput() {
+    this.#parentElement.querySelector('.search__field').value = '';
   }
 
   addHandlerSearch(handler) {
