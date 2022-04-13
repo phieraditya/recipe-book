7ead8b5c598d0fa2ab6f220b40f503e68e60b364
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-04-13 16:01:47 +0700

    implement pagination: scenario (conditional)

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable'; // Polyfilling else
 import 'regenerator-runtime/runtime'; // Polyfilling async-await
@@ -40,8 +41,10 @@ const controlSearchResults = async function () {
 
     // 3) Render results
     // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage(1));
 
-    resultsView.render(model.getSearchResultsPage());
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -5,9 +5,29 @@ class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
   _generateMarkUp() {
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+    console.log(numPages);
+
     // Page 1, and there are other pages
-    // Page 1, and there are NO other pages
+    if (this._data.page === 1 && numPages > 1) {
+      return 'page 1 and others';
+    }
+
     // Last page
+    if (this._data.page === numPages && numPages > 1) {
+      return 'last page';
+    }
+
     // Other page
+    if (this._data.page < numPages) {
+      return 'other page';
+    }
+
+    // Page 1, and there are NO other pages
+    return 'only 1 page';
   }
 }
+
+export default new PaginationView();
