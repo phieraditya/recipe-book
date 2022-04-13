be6ceabffe3dd9de50dab262706fd35132f49810
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-04-13 18:36:19 +0700

    implement pagination: render correct page result and pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -41,7 +41,7 @@ const controlSearchResults = async function () {
 
     // 3) Render results
     // resultsView.render(model.state.search.results);
-    resultsView.render(model.getSearchResultsPage(4));
+    resultsView.render(model.getSearchResultsPage());
 
     // 4) Render initial pagination buttons
     paginationView.render(model.state.search);
@@ -51,7 +51,11 @@ const controlSearchResults = async function () {
 };
 
 const controlPagination = function (goToPage) {
-  console.log(goToPage);
+  // 1) Render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 4) Render NEW pagination buttons
+  paginationView.render(model.state.search);
 };
 
 const init = function () {
--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -11,7 +11,6 @@ class PaginationView extends View {
       if (!btn) return;
 
       const goToPage = +btn.dataset.goto;
-
       handler(goToPage);
     });
   }
@@ -21,7 +20,6 @@ class PaginationView extends View {
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
-    console.log(numPages);
 
     // Page 1, and there are other pages
     if (curPage === 1 && numPages > 1) {
