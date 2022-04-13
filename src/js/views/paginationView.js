c74e2eb6c7d60e8b3d765959344d0218f63f453e
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-04-13 15:32:16 +0700

    implement pagination: scenario

---
+++ src/js/views/paginationView.js
@@ -0,0 +1,13 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  _generateMarkUp() {
+    // Page 1, and there are other pages
+    // Page 1, and there are NO other pages
+    // Last page
+    // Other page
+  }
+}
