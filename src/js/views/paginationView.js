cdc6b268b1fbdc2948053b5bf35dd8ce7516dfbe
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-04-13 16:13:55 +0700

    implement pagination: scenario (buttons markup)

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -5,28 +5,56 @@ class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
   _generateMarkUp() {
+    const curPage = this._data.page;
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
     console.log(numPages);
 
     // Page 1, and there are other pages
-    if (this._data.page === 1 && numPages > 1) {
-      return 'page 1 and others';
+    if (curPage === 1 && numPages > 1) {
+      return `
+        <button class="btn--inline pagination__btn--next">
+          <span>Page ${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
     }
 
     // Last page
-    if (this._data.page === numPages && numPages > 1) {
-      return 'last page';
+    if (curPage === numPages && numPages > 1) {
+      return `
+        <button class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${curPage - 1}</span>
+        </button>
+      `;
     }
 
     // Other page
-    if (this._data.page < numPages) {
-      return 'other page';
+    if (curPage < numPages) {
+      return `
+        <button class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${curPage - 1}</span>
+        </button>
+        <button class="btn--inline pagination__btn--next">
+          <span>Page ${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+    `;
     }
 
     // Page 1, and there are NO other pages
-    return 'only 1 page';
+    return '';
   }
 }
 
