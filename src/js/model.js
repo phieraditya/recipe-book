e8f781297af02c3401686e4566db02804cdca7ec
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-04-18 14:56:54 +0700

    bookmark a recipe
    -create a bookmarks object (an array)
    -push the recipe object to the bookmark array when the user adds a bookmark
    -and set new properties in the recipe object (bookmarked flag)
    
    -create a new controller for adding a new bookmark
    -add event delegation listening for click on the 'btn--bookmark' element
    !the element does not exist when we first load the page, so it is impossible to add an event listener directly to the element
    -bind handler to the listener

--- src/js/model.js
+++ src/js/model.js
@@ -9,6 +9,7 @@ export const state = {
     page: 1,
     resultsPerPage: RES_PER_PAGE,
   },
+  bookmarks: [],
 };
 
 export const loadRecipe = async function (id) {
@@ -69,4 +70,12 @@ export const updateServings = function (newServings) {
   });
 
   state.recipe.servings = newServings;
+};
+
+export const addBookmark = function (recipe) {
+  // Add bookmark
+  state.bookmarks.push(recipe);
+
+  // Mark current recipe as bookmark
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
 };
