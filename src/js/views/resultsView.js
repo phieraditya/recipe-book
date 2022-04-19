2cd93a1924edbca199ead80f655cfd68b1de196a
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-04-19 14:22:40 +0700

    create a parent class of preview
    use PreviewView as a child class of BookmaksView and ResultsView

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,4 +1,5 @@
 import View from './View.js';
+import previewView from './previewView.js';
 import icons from 'url:../../img/icons.svg';
 
 class ResultsView extends View {
@@ -8,27 +9,9 @@ class ResultsView extends View {
 
   _generateMarkUp() {
     console.log(this._data);
-    return this._data.map(this._generateMarkupPreview).join('');
-  }
-
-  _generateMarkupPreview(result) {
-    const id = window.location.hash.slice(1);
-
-    return `
-    <li class="preview">
-      <a href="#${result.id}" class="preview__link ${
-      result.id === id ? 'preview__link--active' : ''
-    }">
-        <figure class="preview__fig">
-          <img src="${result.image}" alt="${result.title}" />
-        </figure>
-        <div class="preview__data">
-          <h4 class="preview__title">${result.title}</h4>
-          <p class="preview__publisher">${result.publisher}</p>
-        </div>
-      </a>
-    </li>
-  `;
+    return this._data
+      .map((result) => previewView.render(result, false))
+      .join('');
   }
 }
 
