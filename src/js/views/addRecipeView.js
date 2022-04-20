214ced0cd053b9a9120da581fa10d8210d3e67e0
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-04-20 16:00:30 +0700

    show form (modal window) to add a recipe
    run the function here in the constructor as soon as the object is created
    !nothing special happens here that the controller needs to tell us
    
    find error: usage of this keyword inside of a handler
    solution: export the function into another method and then call that method with the correct this keyword bound to it

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,28 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+  }
+
+  toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
+
+  _addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
+  }
+  _generateMarkUp() {}
+}
+
+export default new AddRecipeView();
