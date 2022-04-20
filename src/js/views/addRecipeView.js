33d2ca1763f024867de9fbc207c54af556b602e5
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-04-20 16:06:22 +0700

    close window

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -12,6 +12,7 @@ class AddRecipeView extends View {
   constructor() {
     super();
     this._addHandlerShowWindow();
+    this._addHandlerHideWindow();
   }
 
   toggleWindow() {
@@ -22,6 +23,12 @@ class AddRecipeView extends View {
   _addHandlerShowWindow() {
     this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
   }
+
+  _addHandlerHideWindow() {
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
   _generateMarkUp() {}
 }
 
